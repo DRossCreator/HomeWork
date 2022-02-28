8869eb4f65003572e9280c205747bc07b2358b43
Daniil <vk.daniil@mail.ru>
2022-02-28 18:31:56 +0500

    Add project.

---
+++ IOS5-HW10 - YArkovenko Daniil/IOS5-HW10 - YArkovenko Daniil/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS5-HW10 - YArkovenko Daniil
+//
+//  Created by Daniil Yarkovenko on 28.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
